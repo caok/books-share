489045524ecf2c1d760cd11be7a3c3a17510a87e
caok <caok1231@gmail.com>
2013-03-30 23:33:56 +0800

    modify the show of index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,6 @@ module ApplicationHelper
     link_to '&times;'.html_safe, '#', :class => 'close', 'data-dismiss' => dismiss
   end
 
-  def body_attributes
-    class_attributes = [user_signed_in? ? 'signed_in' : 'signed_out']
-    {
-      :class =>  class_attributes
-    }
-  end
-
   def render_user_bar
     if user_signed_in?
       render 'signed_in_user_bar'
@@ -45,5 +38,16 @@ module ApplicationHelper
       html += content_tag(:i, '', :class => 'icon-tag') + tag.name + ' '
     end
     html.html_safe
+  end
+
+  MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                        'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                        'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                        'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                        'webos|amoi|novarra|cdm|alcatel|pocket|iphone|mobileexplorer|mobile'
+  def mobile?
+    agent_str = request.user_agent.to_s.downcase
+    return false if agent_str =~ /ipad/
+    agent_str =~ Regexp.new(MOBILE_USER_AGENTS)
   end
 end
