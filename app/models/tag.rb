b59b39a4ce71ab3ee5719489e6cc7ac1deefef46
caok <caok1231@gmail.com>
2013-04-01 23:04:49 +0800

    modify form and the show of tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,10 @@
 class Tag < ActiveRecord::Base
   attr_accessible :name
 
+  # assocation
+  has_many :relationships
+  has_many :books, :through => :relationships
+
   # validation
   validates :name, :presence => true
 end
