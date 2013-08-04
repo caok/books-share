a75fc0d82743f59ba9ce908e746e48ee5c83c005
songjiayang <songjiayang1@gmail.com>
2013-08-04 18:08:41 +0800

    初步完成微薄登录功能

--- config/initializers/weibo.rb
+++ config/initializers/weibo.rb
@@ -1,3 +1,3 @@
 WeiboOAuth2::Config.api_key = '618142137'
 WeiboOAuth2::Config.api_secret = '372b9c6d303dc4a038740326305d00b7'
-WeiboOAuth2::Config.redirect_uri = 'http://tao.weidaxue.me:3000/weibo/callback'
+WeiboOAuth2::Config.redirect_uri = 'http://tao.weidaxue.me/weibo/callback'
