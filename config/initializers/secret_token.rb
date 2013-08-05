224c7cba16c9d3c6f5558314ce2672939325a836
caok <caok1231@gmail.com>
2013-08-05 13:06:30 +0800

    remove application.yml and secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BooksShare::Application.config.secret_token = '1d295cf6802b73d61dddbd22dd95befb4930669e2c4b3dd25206758806f35a371aff52c4dc28d3ef72158064b45aa80ea34b5d1d2341ab357a6ce7c5cac71aac'
